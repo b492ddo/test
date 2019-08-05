2f8167ccca58f7c492eb290eb37572767ea320dc
n288TJYRX <47216093+n288TJYRX@users.noreply.github.com>
2019-08-05 15:47:13 +0100

    Update script2.py

--- script2.py
+++ script2.py
@@ -1,3 +1,7 @@
-def run(data):
+def run(data, parameters):
+    print ('parameters for this script are: ', parameters)
     data = data + 'Added some data to the end'
+    x = 0
+    for i in range(1000000000):
+        x = x + 1
     return data
