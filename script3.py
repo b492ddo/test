3053810bd39d2e10f6b794f39fc5b81e30c895f2
c015dariu <47317152+c015dariu@users.noreply.github.com>
2019-08-30 08:58:03 +0100

    Update script3.py

--- script3.py
+++ script3.py
@@ -1,6 +1,7 @@
 def run(data, parameters):
     print ('parameters for this script are: ', parameters)
-    if parameters['animal'] is 'cat':
+    parameters = eval(parameters)
+    if parameters["animal"] is "cat":
         data = "<img crossorigin='anonymous' src='https://upload.wikimedia.org/wikipedia/commons/thumb/6/66/An_up-close_picture_of_a_curious_male_domestic_shorthair_tabby_cat.jpg/1280px-An_up-close_picture_of_a_curious_male_domestic_shorthair_tabby_cat.jpg' class='jpg mw-mmv-dialog-is-open' alt='' width='639' height='639' style=''>"
     else:
         data = "<img crossorigin='anonymous' src='https://cdn.pixabay.com/photo/2018/05/07/10/48/husky-3380548__340.jpg' class='jpg mw-mmv-dialog-is-open' alt='' width='639' height='639' style=''>"
