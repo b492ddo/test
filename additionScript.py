a218dbaff1412e2c07db2408fe0cdad511dc237f
g609bmsma <g609bmsma@protonmail.com>
2020-01-21 14:53:43 +0000

    back to list

--- additionScript.py
+++ additionScript.py
@@ -1,4 +1,4 @@
 def run(data, parameters):
     print('parameters for this script are: ', parameters)
     returnData = int(data[2]) + 10
-    return str(returnData)
+    return list(str(returnData))
