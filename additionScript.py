764e01aaf06f17e44099a1d00c492d267f3dc7d3
g609bmsma <g609bmsma@protonmail.com>
2020-01-21 09:35:04 +0000

    simple maths scripts added

---
+++ additionScript.py
@@ -0,0 +1,4 @@
+def run(parameters):
+    print('parameters for this script are: ', parameters)
+    returnData = parameters + 10
+    return returnData
---
+++ multiplicationScript.py
@@ -0,0 +1,4 @@
+def run(parameters):
+    print('parameters for this script are: ', parameters)
+    returnData = parameters * 2
+    return returnData
