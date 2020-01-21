34b3ad63dbc5bb348f91d5533a0a949052b39000
g609bmsma <g609bmsma@protonmail.com>
2020-01-21 16:01:38 +0000

    print added

--- additionScript.py
+++ additionScript.py
@@ -1,5 +1,6 @@
 def run(data, parameters):
     print('parameters for this script are: ', parameters)
     data = list(map(int, data))
+    print(data)
     returnData = data + 10
     return str(returnData)
