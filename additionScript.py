c7901a59ecc3cd447b8b8aa3f6f3200f72421e62
g609bmsma <g609bmsma@protonmail.com>
2020-01-21 14:23:02 +0000

    output now string

--- additionScript.py
+++ additionScript.py
@@ -1,4 +1,5 @@
 def run(data, parameters):
     print('parameters for this script are: ', parameters)
     returnData = int(data[2]) + 10
+    str(returnData)
     return returnData
--- multiplicationScript.py
+++ multiplicationScript.py
@@ -1,4 +1,5 @@
 def run(data, parameters):
     print('parameters for this script are: ', parameters)
     returnData = int(data[2]) * 2
+    str(returnData)
     return returnData
