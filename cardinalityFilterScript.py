63e3f0d5f073dbec346b062e48f278d971106c68
n288TJYRX <47216093+n288TJYRX@users.noreply.github.com>
2020-02-26 11:20:54 +0000

    Update cardinalityFilterScript.py

--- cardinalityFilterScript.py
+++ cardinalityFilterScript.py
@@ -1,5 +1,40 @@
+import json
+
 def run(data, parameters):
-    print(parameters)
-    print(parameters['minCardinality'])
-    print(parameters['maxCardinality'])
-    return str(data)
+    listDictionaries = json.loads(data)
+    minCardinality = int(parameters['minCardinality'])
+    maxCardinality = int(parameters['maxCardinality'])
+    print('minCardinality is: ' + str(minCardinality))
+    print('maxCardinality is: ' + str(maxCardinality))
+
+    # Filter out the elements with a cardinality not within the minimum and maximum
+    for element in listDictionaries[:]:
+        cardinality = getCardinality(element)
+        print('element is: ' + str(element))
+        print('cardinality is: ' + str(cardinality))
+        if cardinality is not None:
+            if (cardinality > maxCardinality) or (cardinality < minCardinality):
+                print('removing')
+                listDictionaries.remove(element)
+            else:
+                print('not removing')
+                continue
+        else:
+            print('not removing')
+            continue
+
+    print('number of elements is: ' + str(len(listDictionaries)))
+    for element in listDictionaries:
+        cardinality = getCardinality(element)
+        print('cardinality left is: ' + str(cardinality))
+    return json.dumps(listDictionaries)
+
+def getCardinality(element):
+    try:
+        cardinality = int(element['properties']['hllp']['com.clearspring.analytics.stream.cardinality.HyperLogLogPlus']['hyperLogLogPlus']['cardinality'])
+    except KeyError:
+        cardinality = None
+
+    # print('cardinality is: ' + str(cardinality))
+    # print('type of cardinality is: ' + str(type(cardinality)))
+    return cardinality
