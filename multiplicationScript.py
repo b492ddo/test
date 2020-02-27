11d802abaed7930ae01340097422ed35d89cefbe
n288TJYRX <47216093+n288TJYRX@users.noreply.github.com>
2020-02-27 14:21:57 +0000

    Update multiplicationScript.py

--- multiplicationScript.py
+++ multiplicationScript.py
@@ -4,4 +4,4 @@ def run(data, parameters):
     print('parameters for this script are: ', parameters)
     dictData = json.loads(data)
     dictData['testKey'] = str(int(dictData['testKey']) * 2)
-    return dictData
+    return json.dumps(dictData)
